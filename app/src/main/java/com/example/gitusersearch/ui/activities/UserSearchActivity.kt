442b1ae3433b2c52a9e11b667b93bce42bb1ec27
recker <sursantanu614@gmail.com>
2019-01-30 23:33:46 -0800

    cached all searched queries to the content provider

--- app/src/main/java/com/example/gitusersearch/ui/activities/UserSearchActivity.kt
+++ app/src/main/java/com/example/gitusersearch/ui/activities/UserSearchActivity.kt
@@ -1,16 +1,21 @@
 package com.example.gitusersearch.ui.activities
 
+import android.app.SearchManager
+import android.content.Intent
 import android.os.Bundle
+import android.provider.SearchRecentSuggestions
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.gitusersearch.ContentProviders.MySuggestionProvider
 import com.example.gitusersearch.R
 import com.example.gitusersearch.SearchAppClass
 import com.example.gitusersearch.di.DaggerSearchActivityComponent
 import com.example.gitusersearch.di.SeachActivityModule
 import com.example.gitusersearch.ui.adapters.RvRepositoryAdapter
 import com.example.gitusersearch.viewModel.SearchViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 
@@ -36,9 +41,19 @@ class UserSearchActivity : AppCompatActivity() {
     }
 
     private fun initMembers() {
-
+        rvRepositories.adapter = rvRepositoryAdapter
+        rvRepositories.layoutManager = layoutManager
     }
 
+    override fun onNewIntent(intent: Intent?) {
+        if (Intent.ACTION_SEARCH == intent?.action) {
+            intent.getStringExtra(SearchManager.QUERY)?.also { query ->
+                SearchRecentSuggestions(this, MySuggestionProvider.AUTHORITY, MySuggestionProvider.MODE)
+                    .saveRecentQuery(query, null)
+            }
+        }
+        super.onNewIntent(intent)
+    }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.menu, menu)
